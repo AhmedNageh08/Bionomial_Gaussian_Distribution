ca57929f44727fad5b4851dd9db2a2390867c2d0
Ahmed Nageh <ahmednageh08@gmail.com>
2020-04-27 23:24:24 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Bio_Gaus',
+      version='0.1',
+      description='Gaussian and Bionomial distributions',
+      packages=['Bio_Gaus'],
+      author='Ahmed Nageh',
+      author_email='ahmednageh08@gmail.com',
+      zip_safe=False)
